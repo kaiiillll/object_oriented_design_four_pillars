ec7c79f11617faa7a60b4e76790938f6bf2eb81e
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-05-20 14:29:09 +0800

    hierarchal_type

--- base_app.py
+++ base_app.py
@@ -1,2 +1,10 @@
 # This base will be the parent of the other classes
 # Import tkinter as a base app
+import tkinter as tk
+
+class BaseApp(tk.Tk):
+    def __init__(self, title, size, bg_color="white"):
+        super().__init__()
+        self.title(title)
+        self.geometry(size)
+        self.configure(bg=bg_color)
