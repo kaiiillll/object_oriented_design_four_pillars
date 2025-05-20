901309426eaaf97114a7deb1a6f6d87bf30ce186
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-05-20 18:16:39 +0800

    imports_and_child_class

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,3 +1,12 @@
 # import the needed function from the base app 
+from base_app import BaseApp
+import tkinter as tk
+from tkinter import messagebox
+
+class QuizCreator(BaseApp):
+    def __init__(self):
+        super().__init__("Teacher's Quiz Maker", "500x550", "#e6f2ff")
+        self.build_ui()
+
 # need to make class to be under the parent which is the base 
 # then proceed to copy all the remaining codes from my original 
