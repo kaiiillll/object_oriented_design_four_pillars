0d1c5ce762b47a18129f38b5a1af23ec52122be4
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-05-22 04:40:53 +0800

    child_class_function

--- quiz_runner.py
+++ quiz_runner.py
@@ -3,3 +3,15 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 import os
+
+# function class to be under the parent
+class BrainRouletteApp(BaseApp):
+    def __init__(self):
+        super().__init__("Brain Roulette Game", "800x600", "white")
+
+        self.players = []
+        self.category = None
+        self.question_manager = QuestionManager()
+
+        self.current_frame = None
+        self.show_intro()
