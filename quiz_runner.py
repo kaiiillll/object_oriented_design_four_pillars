637c7b7b0a4857d85623cb1a070162f3932602f4
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-05-23 10:15:48 +0800

    class_changes

--- quiz_runner.py
+++ quiz_runner.py
@@ -5,29 +5,20 @@ import random
 import os
 
 # function class to be under the parent
-class BrainRouletteApp(BaseApp):
+class BaseApp(tk.Tk):
+    def __init__(self, title, geometry, bg_color):
+        super().__init__()
+        self.title(title)
+        self.geometry(geometry)
+        self.configure(bg=bg_color)
+
+class QuestionManager:
     def __init__(self):
-        super().__init__("Brain Roulette Game", "800x600", "white")
+        self.questions = []  # Load your questions here
 
-        self.players = []
-        self.category = None
-        self.question_manager = QuestionManager()
-
-        self.current_frame = None
-        self.show_intro()
+class IntroAnimation(tk.Frame):
+    def __init__(self, parent, next_callback):
+        super().__init__(parent)
+        tk.Label(self, text="Welcome to Brain Roulette!").pack()
+        tk.Button(self, text="Continue", command=next_callback).pack()
         
-# registration and animation
-    def clear_frame(self):
-        if self.current_frame:
-            self.current_frame.destroy()
-            self.current_frame = None
-
-    def show_intro(self):
-        self.clear_frame()
-        self.current_frame = IntroAnimation(self, self.show_player_registration)
-        self.current_frame.pack(expand=True, fill="both")
-
-    def show_player_registration(self):
-        self.clear_frame()
-        self.current_frame = PlayerRegistration(self, self.start_main_menu)
-        self.current_frame.pack(expand=True, fill="both")
